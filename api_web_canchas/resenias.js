a18ddf44fbc2e91264f1160de630449100b3c541
Andrea Holland <andreaholland.lr@gmail.com>
2023-10-25 15:27:16 -0300

    comentarios con cosultas SQL

--- api_web_canchas/resenias.js
+++ api_web_canchas/resenias.js
@@ -1,3 +1,26 @@
 import express from "express";
+import { db } from "./db.js";
+
+/*
+CREATE TABLE resenias (
+    id_resenia INT PRIMARY KEY AUTO_INCREMENT,
+    reserva_id INT NOT NULL,
+    comentario VARCHAR(255) NOT NULL,
+    calificacion INT NOT NULL,
+    CONSTRAINT fk_reservas_resenias -- Relación entre `reseñas` y `reservas`
+    FOREIGN KEY (reserva_id) REFERENCES reservas (id_reserva)
+  );
+*/
+
+
+
+
+
+
+
+
+
+
 
 export const reseniasRouter = express.Router();
+
