062b1764891c964f4b81f7eaa8342b9bedd56c92
Andrea Holland <andreaholland.lr@gmail.com>
2023-10-25 18:24:23 -0300

    Comando SQL en accesorios.js y nueva rama

--- api_web_canchas/accesorios.js
+++ api_web_canchas/accesorios.js
@@ -1,3 +1,14 @@
 import express from "express";
 
+/*
+CREATE TABLE accesorios (
+  id_accesorio INT PRIMARY KEY AUTO_INCREMENT,
+  nombre VARCHAR(60) NOT NULL,
+  tipo_deporte VARCHAR(60) NOT NULL,
+  precio INT NOT NULL,
+  estado VARCHAR(45),
+  FOREIGN KEY (tipo_deporte) REFERENCES canchas (tipo_deporte)
+);
+*/
+
 export const accesoriosRouter = express.Router();
