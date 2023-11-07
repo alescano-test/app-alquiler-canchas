b3a43fca345d9d1c6ec112e1f457fd7d1fde65dd
Andrea Holland <andreaholland.lr@gmail.com>
2023-11-06 22:26:06 -0300

    se agrega botón

--- ui_web_canchas/src/App.jsx
+++ ui_web_canchas/src/App.jsx
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import {SignIn}  from "./SignInPage";
 import {HomePage} from "./HomePage"
 import { Layout } from "./Layout";
+import { Button } from "@mui/material"
 
 export default function App() {
   return (
     <>
     <h1>Hola</h1>
+    <Button>Iniciar sesión</Button>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />}/>
--- ui_web_canchas/src/main.jsx
+++ ui_web_canchas/src/main.jsx
@@ -3,6 +3,12 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import React from "react";
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
       <BrowserRouter>
