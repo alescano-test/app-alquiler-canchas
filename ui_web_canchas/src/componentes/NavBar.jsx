23947684a46c693e724658dd656048ac94687526
Alejandro Lescano <alescano.test@gmail.com>
2023-11-20 22:43:50 -0300

    funciona

--- ui_web_canchas/src/componentes/NavBar.jsx
+++ ui_web_canchas/src/componentes/NavBar.jsx
@@ -1,21 +1,22 @@
-import { Link } from "react-router-dom"
-import logo_nombre from "../img/logo_nombre.png"
-import { Boton } from "./Boton"
+import { Link } from "react-router-dom";
+import logo_nombre from "../img/logo_nombre.png";
+import { Boton } from "./Boton";
 
 export const Navbar = () => {
-    return(
-        <>
-        <div className="contenedor-navbar bg-verde-oscuro flex justify-between" >
-            <div className="contenedor-img ms-4">
-                <Link href= "/">
-                    <img src={logo_nombre} />
-                </Link>
-            </div>
-            <div className="self-center mr-3">
-                <Boton btnNombre="Iniciar Sesión" />
-            </div>
-
+  return (
+    <>
+      <div className="contenedor-navbar bg-verde-oscuro flex justify-between">
+        <div className="contenedor-img ms-4">
+          <Link to="/">
+            <img src={logo_nombre} />
+          </Link>
         </div>
-        </>
-    )
-}
+        <div className="self-center mr-3">
+          <Link to="/login">
+            <Boton btnNombre="Iniciar Sesión" />
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
